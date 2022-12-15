210e1f0c89ddfa37f75f40082265e9e196f5c050
Charles-dev-ai <118250283+Charles-dev-ai@users.noreply.github.com>
2022-12-16 01:54:25 +0300

    Add files via upload

---
+++ java/com/example/myapplication/ServerConnection.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//THIS CLASS ESTABLISHES CONNECTION
+
+public class ServerConnection
+{
+    public static  final String SERVER_URL="https://197.248.5.24";
+
+    private static Retrofit retrofit=null;
+    // connection
+    public static Retrofit getServerConnection()
+    {
+        if(retrofit==null)
+        {
+            retrofit=new Retrofit.Builder().baseUrl(SERVER_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return  retrofit;
+    }
+}
+//Place root path followed by '/',
